6bf1eb646ed9e05c8bde4256882601c24a462121
kjwjwjw <tbs03112@naver.com>
2024-12-27 21:44:59 +0900

    알고리즘 풀이

---
+++ Study/src/soul10.java
@@ -0,0 +1,59 @@
+import java.util.*;
+/*
+설명
+
+문자와 숫자가 섞여있는 문자열이 주어지면 그 중 숫자만 추출하여 그 순서대로 자연수를 만듭니다.
+
+만약 “tge0a1h205er”에서 숫자만 추출하면 0, 1, 2, 0, 5이고 이것을 자연수를 만들면 1205이 됩니다.
+
+추출하여 만들어지는 자연수는 100,000,000을 넘지 않습니다.
+
+
+입력
+
+첫 줄에 숫자가 섞인 문자열이 주어집니다. 문자열의 길이는 100을 넘지 않습니다.
+
+
+출력
+
+첫 줄에 자연수를 출력합니다.
+ */
+class Main10 {
+    public int[] soultion(String s, char t) {
+      int[] answer= new int[s.length()];
+      int p=1000;
+      for(int i=0; i<s.length();i++) {
+          if(s.charAt(i) ==t) {
+              p=0;
+              answer[i] =p;
+          }
+          else {
+              p++;
+              answer[i] = p;
+          }
+      }
+      p=1000;
+      for(int i=s.length()-1 ; i>=0; i--) {
+          if (s.charAt(i) == t) {
+              p = 0;
+
+          } else {
+              p++;
+              answer[i] = Math.min(answer[i], p);
+          }
+      }
+
+      return answer;
+    }
+
+    public static void main(String[] args) {
+        Main10 T = new Main10();
+        Scanner kb = new Scanner(System.in);
+        String str=kb.next();
+        char c =kb.next().charAt(0);
+        for(int x : T.soultion(str, c )) {
+            System.out.print(x+" ");
+        }
+
+    }
+}
